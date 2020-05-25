dafc02298ac8bfdaa1332e5ffb5d92b7ea575a9f
Abhinav Tyagi <reach2abhinavtyagi@gmail.com>
2020-05-26 02:24:45 +0530

    Updates

---
+++ precache-manifest.17c8e324dcdb8f0d9c2462095b05c6c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8adf136983d8a85852cf30184d71f9bd",
+    "url": "//github.com/abhinav-08/Contact-manager-app-React-/index.html"
+  },
+  {
+    "revision": "b73292c57a5587726e3f",
+    "url": "//github.com/abhinav-08/Contact-manager-app-React-/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e6703e783495e7fec55e",
+    "url": "//github.com/abhinav-08/Contact-manager-app-React-/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "b73292c57a5587726e3f",
+    "url": "//github.com/abhinav-08/Contact-manager-app-React-/static/js/2.36bd952f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "//github.com/abhinav-08/Contact-manager-app-React-/static/js/2.36bd952f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6703e783495e7fec55e",
+    "url": "//github.com/abhinav-08/Contact-manager-app-React-/static/js/main.2cdb0d8f.chunk.js"
+  },
+  {
+    "revision": "750f9b592d9745b7bfb3",
+    "url": "//github.com/abhinav-08/Contact-manager-app-React-/static/js/runtime-main.8c174c46.js"
+  }
+]);
